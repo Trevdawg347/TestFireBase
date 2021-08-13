67ef2a6d8e0017c950308c0fce9ba05266f734af
Trevor <tbuhler347@gmail.com>
2021-08-12 21:09:24 -0600

    Initial Commit

---
+++ TestFireBase/TestFireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFireBaseApp.swift
+//  TestFireBase
+//
+//  Created by Wendy Buhler on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TestFireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
